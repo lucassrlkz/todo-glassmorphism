1897b720994c3d90df4330d6c1d9bae55a8ca6fa
Lucas Vieira Silva <lucas_vieira97@hotmail.com>
2021-12-07 17:08:34 -0300

    update check btn

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,30 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
-import { FaTrashAlt } from 'react-icons/fa';
+import React from "react";
+import styled from "styled-components";
+import { FaTrashAlt, FaCheck } from "react-icons/fa";
 
-export default function Todo({ todo, index, completeTodo, removeTodo, }) {
-    
+export default function Todo({ todo, index, completeTodo, removeTodo }) {
   return (
-    <TodoStyled style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}>
+    <TodoStyled
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
       {todo.text}
       <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <FaTrashAlt onClick={() => removeTodo(index)} style={{ color: "whitesmoke" }} />
+        <FaCheck
+          onClick={() => completeTodo(index)}
+          style={{ color: "whitesmoke" }}
+        />
+        <FaTrashAlt
+          onClick={() => removeTodo(index)}
+          style={{ color: "whitesmoke", marginLeft: "20px" }}
+        />
       </div>
     </TodoStyled>
   );
 }
 const TodoStyled = styled.div`
-    display:flex;
-    flex-direction:row;
-    justify-content: space-between;
-    justify-items: flex-start;
-    width:300px; 
-  
-    padding: .5rem;
-    margin-bottom:.5rem;
-    background: linear-gradient(to right top, rgba(255, 255, 255, 5%), rgba(255, 255, 255, 30%));
-    color: black;
-    border-radius: .5rem;
-    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.15);
-  `; 
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  justify-items: flex-start;
+  width: 300px;
+
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+  background: linear-gradient(
+    to right top,
+    rgba(255, 255, 255, 5%),
+    rgba(255, 255, 255, 30%)
+  );
+  color: black;
+  border-radius: 0.5rem;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.15);
+`;
